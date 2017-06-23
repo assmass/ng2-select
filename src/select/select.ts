cf9e725f9fad6f72fe55781a4552c3cbffb77066
Anderson <anderson@mikweb.com.br>
2017-06-23 15:51:41 -0300

    Alinhamento icon allowClear - right

--- src/select/select.ts
+++ src/select/select.ts
@@ -137,7 +137,7 @@ let styles = `
               [innerHTML]="sanitize(active[0].text)"></span>
         <i class="dropdown-toggle pull-right"></i>
         <i class="caret pull-right"></i>
-        <a *ngIf="allowClear && active.length>0" class="btn btn-xs btn-link pull-right" style="margin-right: 10px; padding: 0;" (click)="removeClick(active[0], $event)">
+        <a *ngIf="allowClear && active.length>0" class="btn btn-xs btn-link float-right" style="margin-right: 10px; padding: 0;" (click)="removeClick(active[0], $event)">
            <i class="icon-close"></i>
         </a>
       </span>
